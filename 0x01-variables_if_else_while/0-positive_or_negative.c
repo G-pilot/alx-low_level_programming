5c8ffa5ad383e395526e88b26e0cf115e7454704
G-pilot <gregoryogechukwu7@gmail.com>
2023-06-19 01:39:51 -0700

    c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,38 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ *   This program assigns a random number to the variable 'n' each time it is executed.
+ *   It then prints the last digit of the number stored in 'n' and provides additional information based on its value.
+ *
+ * Return:
+ *   Always returns 0 to indicate successful execution.
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	n = rand();
+
+	printf("Last digit of %d is %d ", n, n % 10);
+
+	if (n % 10 > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (n % 10 == 0)
+	{
+		printf("and is 0\n");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+
 	return (0);
 }
