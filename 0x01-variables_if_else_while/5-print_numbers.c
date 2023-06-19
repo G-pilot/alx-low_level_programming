20fdc52de03f4390f3736b3572e676997e20a123
G-pilot <gregoryogechukwu7@gmail.com>
2023-06-19 03:08:47 -0700

    c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,7 +4,7 @@
  *
  * Description:
  *   This program prints all single-digit numbers of base 10, starting from 0,
- *   followed by a new line. It utilizes the putchar function to print individual characters.
+ *   followed by a new line.
  *
  * Return:
  *   Always returns 0 to indicate successful execution.
